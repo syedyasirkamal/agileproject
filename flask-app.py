a0aa234d8a2957eb940b0921dba8c414db002013
syed <syedyasirkamal@gmail.com>
2022-12-07 10:43:05 -0500

    quiz update

--- flask-app.py
+++ flask-app.py
@@ -1,20 +1,24 @@
 #          Import some packages               #
 ###############################################
 import os
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, abort
+from dotenv import load_dotenv
+
 from app.forms import signupForm, trialForm
 from flask_mysqldb import MySQL
 from flask_bootstrap import Bootstrap
 from flask_datepicker import datepicker
 from flask_mail import Mail, Message
+import stripe
 
-
+load_dotenv()
 ###############################################
 #          Define flask app                   #from flask-app import db, Contact
 ###############################################
 
 app = Flask(__name__,template_folder='templates')
 app.secret_key = 'the random string'
+stripe.api_key = os.environ['STRIPE_SECRET_KEY']
 Bootstrap(app)
 datepicker(app)
 
@@ -49,6 +53,101 @@ app.config['MAIL_USE_SSL'] = True
 mail = Mail(app)
 
 
+
+###############################################
+#         Checkout
+###############################################
+
+products = {
+    'megatutorial': {
+        'name': 'The Flask Mega-Tutorial',
+        'price': 3900,
+    },
+    'support': {
+        'name': 'Python 1:1 support',
+        'price': 20000,
+        'per': 'hour',
+        'adjustable_quantity': {
+            'enabled': True,
+            'minimum': 1,
+            'maximum': 4,
+        },
+    },
+}
+
+
+@app.route('/purchase')
+def purchase():
+    return render_template('purchase.html', products=products)
+
+
+@app.route('/order/<product_id>', methods=['POST'])
+def order(product_id):
+    if product_id not in products:
+        abort(404)
+
+    checkout_session = stripe.checkout.Session.create(
+        line_items=[
+            {
+                'price_data': {
+                    'product_data': {
+                        'name': products[product_id]['name'],
+                    },
+                    'unit_amount': products[product_id]['price'],
+                    'currency': 'usd',
+                },
+                'quantity': 1,
+                'adjustable_quantity': products[product_id].get(
+                    'adjustable_quantity', {'enabled': False}),
+            },
+        ],
+        payment_method_types=['card'],
+        mode='payment',
+        success_url=request.host_url + 'order/success',
+        cancel_url=request.host_url + 'order/cancel',
+    )
+    return redirect(checkout_session.url)
+
+
+@app.route('/order/success')
+def success():
+    return render_template('success.html')
+
+
+@app.route('/order/cancel')
+def cancel():
+    return render_template('cancel.html')
+
+
+@app.route('/event', methods=['POST'])
+def new_event():
+    event = None
+    payload = request.data
+    signature = request.headers['STRIPE_SIGNATURE']
+
+    try:
+        event = stripe.Webhook.construct_event(
+            payload, signature, os.environ['STRIPE_WEBHOOK_SECRET'])
+    except Exception as e:
+        # the payload could not be verified
+        abort(400)
+
+    if event['type'] == 'checkout.session.completed':
+      session = stripe.checkout.Session.retrieve(
+          event['data']['object'].id, expand=['line_items'])
+      print(f'Sale to {session.customer_details.email}:')
+      for item in session.line_items.data:
+          print(f'  - {item.quantity} {item.description} '
+                f'${item.amount_total/100:.02f} {item.currency.upper()}')
+
+    return {'success': True}
+
+
+
+###############################################
+#         Other Pages
+###############################################
+
 @app.route('/createdatabase')
 def createdatabase():
     # Creating a connection cursor
@@ -72,6 +171,12 @@ def createdatabase():
 @app.route('/', methods=['GET'])
 def index():
     return render_template("main-container.html")
+
+
+@app.route('/checkout', methods=['GET'])
+def checkout():
+    return render_template("checkout.html")
+
 
 @app.route('/trial', methods=["GET", "POST"])
 def trial():
---
+++ static/scripts/quiz.js
@@ -0,0 +1,202 @@
+var quiz = {
+  // (A) PROPERTIES
+  // (A1) QUESTIONS & ANSWERS
+  // Q = QUESTION, O = OPTIONS, A = CORRECT ANSWER
+  data: [
+  {
+    q : "How do you say \"hello\" in Spanish?",
+    o : [
+      "Hola",
+      "Adios",
+      "Jabon",
+      "Bonjour"
+    ],
+    a : 0 // arrays start with 0
+  },
+  {
+    q : "How do you say \"dog\" in Spanish?",
+    o : [
+      "Gato",
+      "Raton",
+      "Perro",
+      "Sag"
+    ],
+    a : 2
+  },
+  {
+    q : "If you want to ask where the bathroom is what is the correct phrase to use?",
+    o : [
+      "Adonde estamos?",
+      "Adonde esta el bano?",
+      "Y las llaves?",
+      "Podemos ir a la tienda?"
+    ],
+    a : 1
+  },
+  {
+    q : "How do you say \"wash\" in Spanish?",
+    o : [
+      "Pintar",
+      "Lavar",
+      "Ajudar",
+      "Vashar"
+    ],
+    a : 1
+  },
+  {
+    q : "How do you say \"Miss\" in Spanish?",
+    o : [
+      "Chiquita",
+      "Mademoiselle",
+      "Belleza",
+      "Senorita"
+    ],
+    a : 3
+  },
+  {
+    q : "What is the correct phrase for Good Morning?",
+    o : [
+      "Buenas Noches",
+      "Buenas Tardes",
+      "Buenos Dias",
+      "Bonne Nuit"
+    ],
+    a : 2
+  },
+  {
+    q : "If someone asks you in Spanish, Adonde esta la farmacia,? you answer",
+    o : [
+      "Esta en la esquina",
+      "Ven con migo",
+      "Quieres ir a la escuela",
+      "Queremos ir a el parque"
+    ],
+    a : 0
+  },
+  {
+    q : "If someone asks you in Spanish Adonde vas? you answer",
+    o : [
+      "No quiero",
+      "Para esto",
+      "Fuimos al parque",
+      "Para la casa de mi amigo"
+    ],
+    a : 3
+  },
+  {
+    q : "Click the best translation of Piedra:",
+    o : [
+      "Pardon",
+      "Sac",
+      "Stone",
+      "Pier"
+    ],
+    a : 2
+  },
+  {
+    q : "Click the best translation of el cristal:",
+    o : [
+      "Glass",
+      "Cyrstal",
+      "Ball",
+      "Christ"
+    ],
+    a : 0
+  }
+  ],
+
+  // (A2) HTML ELEMENTS
+  hWrap: null, // HTML quiz container
+  hQn: null, // HTML question wrapper
+  hAns: null, // HTML answers wrapper
+
+  // (A3) GAME FLAGS
+  now: 0, // current question
+  score: 0, // current score
+
+  // (B) INIT QUIZ HTML
+  init: () => {
+    // (B1) WRAPPER
+    quiz.hWrap = document.getElementById("quizWrap");
+
+    // (B2) QUESTIONS SECTION
+    quiz.hQn = document.createElement("div");
+    quiz.hQn.id = "quizQn";
+    quiz.hWrap.appendChild(quiz.hQn);
+
+    // (B3) ANSWERS SECTION
+    quiz.hAns = document.createElement("div");
+    quiz.hAns.id = "quizAns";
+    quiz.hWrap.appendChild(quiz.hAns);
+
+    // (B4) GO!
+    quiz.draw();
+  },
+
+  // (C) DRAW QUESTION
+  draw: () => {
+    // (C1) QUESTION
+    quiz.hQn.innerHTML = quiz.data[quiz.now].q;
+
+    // (C2) OPTIONS
+    quiz.hAns.innerHTML = "";
+    for (let i in quiz.data[quiz.now].o) {
+      let radio = document.createElement("input");
+      radio.type = "radio";
+      radio.name = "quiz";
+      radio.id = "quizo" + i;
+      quiz.hAns.appendChild(radio);
+      let label = document.createElement("label");
+      label.innerHTML = quiz.data[quiz.now].o[i];
+      label.setAttribute("for", "quizo" + i);
+      label.dataset.idx = i;
+      label.addEventListener("click", () => { quiz.select(label); });
+      quiz.hAns.appendChild(label);
+    }
+  },
+
+  // (D) OPTION SELECTED
+  select: (option) => {
+    // (D1) DETACH ALL ONCLICK
+    let all = quiz.hAns.getElementsByTagName("label");
+    for (let label of all) {
+      label.removeEventListener("click", quiz.select);
+    }
+
+    // (D2) CHECK IF CORRECT
+    let correct = option.dataset.idx == quiz.data[quiz.now].a;
+    if (correct) {
+      quiz.score++;
+      option.classList.add("correct");
+    } else {
+      option.classList.add("wrong");
+    }
+
+    // (D3) NEXT QUESTION OR END GAME
+    quiz.now++;
+    setTimeout(() => {
+      if (quiz.now < quiz.data.length) { quiz.draw(); }
+      else {
+        quiz.hAns.innerHTML = "Book your trial <a href='/trial'>here</a> or Purchase <a href='/purchase'>now</a>";
+        score_message=`You have answered ${quiz.score} of ${quiz.data.length} correctly.`
+        if (quiz.score <=3){
+          quiz.hQn.innerHTML = score_message + "You seem to be at lower elementary level";
+        } else if (quiz.score > 3 && quiz.score < 6) {
+          quiz.hQn.innerHTML = score_message + "You seem to be at upper elementary level";
+        } else if (quiz.score > 5 && quiz.score < 9) {
+          quiz.hQn.innerHTML = score_message + "You seem to be at lower intermediate level";
+        } else if (quiz.score > 8 && quiz.score <= 10) {
+          quiz.hQn.innerHTML = score_message + "You seem to be at upper intermediate level";
+        }
+      }
+    }, 1000);
+  },
+
+  // (E) RESTART QUIZ
+  reset : () => {
+    quiz.now = 0;
+    quiz.score = 0;
+    quiz.draw();
+  }
+};
+window.addEventListener("load", quiz.init);
