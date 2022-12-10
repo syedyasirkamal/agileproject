1d70b8d3b0dd0a87d89bd25868ab2336e593983d
syed <syedyasirkamal@gmail.com>
2022-12-10 12:42:19 -0500

    BDD commit

--- features/environment.py
+++ features/environment.py
@@ -11,8 +11,8 @@ app=application.app
 
 
 
-# Use the chrome driver specific to your version of Chrome browser and put it in ./driver directory
-CHROME_DRIVER = os.path.join(os.path.join(os.path.dirname(__file__), 'driver'), 'chromedriver')
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 chrome_options = Options()
 chrome_options.add_argument("--headless")
@@ -27,7 +27,7 @@ def before_all(context):
     context.pa_app = threading.Thread(target=context.server.serve_forever)
     context.pa_app.start()
 
-    context.browser = webdriver.Chrome(options=chrome_options, executable_path=CHROME_DRIVER)
+    context.browser = webdriver.Chrome(options=chrome_options, service=Service(ChromeDriverManager().install()))
     context.browser.set_page_load_timeout(time_to_wait=200)
 
 
