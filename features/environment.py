5a929a100f65bb6471da20fe6fccba9e23e06b4d
syed <syedyasirkamal@gmail.com>
2022-12-10 13:37:26 -0500

    BDD commit

--- features/environment.py
+++ features/environment.py
@@ -24,7 +24,7 @@ def before_all(context):
     context.pa_app = threading.Thread(target=context.server.serve_forever)
     context.pa_app.start()
 
-    context.browser = webdriver.Chrome(options=chrome_options)
+    context.browser = webdriver.Chrome(options=chrome_options,executable_path="/")
     context.browser.set_page_load_timeout(time_to_wait=200)
 
 
