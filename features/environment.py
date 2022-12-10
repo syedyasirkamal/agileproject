e61cef13f12e313e445567b03742a80fe6d6b8b5
syed <syedyasirkamal@gmail.com>
2022-12-10 17:11:52 -0500

    BDD commit

--- features/environment.py
+++ features/environment.py
@@ -10,10 +10,7 @@ application = importlib.import_module("flask-app")
 app=application.app
 
 
-
-# Use the chrome driver specific to your version of Chrome browser and put it in ./driver directory
-CHROME_DRIVER = os.path.join(os.path.join(os.path.dirname(__file__), 'driver'), 'chromedriver.exe')
-
+driver = webdriver.Chrome(ChromeDriverManager().install())
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument('--no-proxy-server')
@@ -27,7 +24,7 @@ def before_all(context):
     context.pa_app = threading.Thread(target=context.server.serve_forever)
     context.pa_app.start()
 
-    context.browser = webdriver.Chrome(options=chrome_options, executable_path=CHROME_DRIVER)
+    context.browser = webdriver.Chrome(options=chrome_options, executable_path=driver)
     context.browser.set_page_load_timeout(time_to_wait=200)
 
 
