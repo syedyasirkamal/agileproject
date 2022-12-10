fbdb6e06640c43118d2ff38adeeacca7b6953454
syed <syedyasirkamal@gmail.com>
2022-12-10 16:22:03 -0500

    BDD commit

--- features/environment.py
+++ features/environment.py
@@ -5,6 +5,8 @@ from wsgiref.simple_server import WSGIRequestHandler
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+
+
 from webdriver_manager.chrome import ChromeDriverManager
 import importlib
 application = importlib.import_module("flask-app")
@@ -12,8 +14,6 @@ app=application.app
 
 import chromedriver_autoinstaller
 
-
-ser = Service("C:/Users/syedy/Downloads/chromedriver")
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument('--no-proxy-server')
@@ -27,7 +27,7 @@ def before_all(context):
     context.pa_app = threading.Thread(target=context.server.serve_forever)
     context.pa_app.start()
 
-    context.browser = webdriver.Chrome(options=chrome_options, service=ser)
+    context.browser = webdriver.Chrome(options=chrome_options)
     context.browser.set_page_load_timeout(time_to_wait=200)
 
 
