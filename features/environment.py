745c2f237555b3b8c395223779d30ff4c4336c00
syed <syedyasirkamal@gmail.com>
2022-12-10 15:24:31 -0500

    BDD commit

--- features/environment.py
+++ features/environment.py
@@ -11,9 +11,6 @@ app=application.app
 
 
 
-# Use the chrome driver specific to your version of Chrome browser and put it in ./driver directory
-CHROME_DRIVER = os.path.join(os.path.join(os.path.dirname(__file__), 'driver'), 'chromedriver')
-
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument('--no-proxy-server')
@@ -27,7 +24,7 @@ def before_all(context):
     context.pa_app = threading.Thread(target=context.server.serve_forever)
     context.pa_app.start()
 
-    context.browser = webdriver.Chrome(options=chrome_options, executable_path=CHROME_DRIVER)
+    context.browser = webdriver.Chrome(options=chrome_options, executable_path="C:/path/to/chromedriver.exe")
     context.browser.set_page_load_timeout(time_to_wait=200)
 
 
